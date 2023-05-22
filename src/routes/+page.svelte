6a7ce9d3ef73273d710c8e1d298d2676666c3043
E3TU <99178022+E3TU@users.noreply.github.com>
2023-05-22 20:56:11 +0300

    Modifying sidebar and adding container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,6 @@
   let test = "hello world";
 </script>
 
-<svelte:head>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
-    rel="stylesheet"
-  />
-</svelte:head>
-
 <body>
   <header class="navbar">
     <nav>
@@ -21,25 +14,30 @@
       </ul>
     </nav>
   </header>
+  <div class="container">
+    <h1>Test</h1>
+  </div>
 </body>
 
 <style lang="scss">
-  @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+  @import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
 
   $background-color: #191919;
   $foreground-color: #148a55;
   $white: #ffffff;
+  $black: #000;
 
   * {
     margin: 0;
     padding: 0;
   }
-
   body {
+    // display: flex;
+    // flex-direction: column;
     margin: 0;
     padding: 0;
     background-color: $background-color;
-    font-family: "Poppins", sans-serif;
+    font-family: 'Ubuntu', sans-serif;
   }
   .navbar {
     display: flex;
@@ -53,7 +51,7 @@
   ul {
     display: flex;
     list-style: none;
-     flex-direction: column;
+    flex-direction: column;
     font-size: 1.75rem;
     margin-left: 5rem;
 
@@ -63,7 +61,21 @@
       a {
         text-decoration: none;
         color: $white;
+        transition: 1s;
+        &:hover{
+          color: $background-color;
+          transition: 1s;
+        }
       }
     }
+  }
+  .container{
+    display: flex;
+    align-items: center;
+    background-color: #6c0d0d;
+    width: calc(100% - 25rem);
+    margin-left: 25rem;
+    height: 100%;
+    // flex: 1;
   }
 </style>
