999467a25d21e8ddc6b30f12082aa512694f3406
E3TU <99178022+E3TU@users.noreply.github.com>
2023-05-27 19:54:06 +0300

    Home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,79 +3,133 @@
 </script>
 
 <body>
-  <header class="navbar">
-    <nav>
+  <div class="flex-container">
+    <div class="navbar">
       <ul>
         <li><a href="#">Home</a></li>
-        <li><a href="#">About me</a></li>
         <li><a href="#">Skills</a></li>
+        <li><a href="#">About</a></li>
         <li><a href="#">Projects</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
-    </nav>
-  </header>
-  <div class="container">
-    <h1>Test</h1>
+    </div>
+    <div class="container">
+      <div class="introduction">
+        <img class="profilepic" src="default-profile.png">
+        <h1 class="name">E3TU</h1>
+        <h1 class="profession">Software Developer</h1>
+        <a href="https://github.com/E3TU" class="github-btn">Check My Github</a>
+      </div>
+    </div>
+    <div class="container2">
+      
+    </div>
   </div>
 </body>
 
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
-
   $background-color: #191919;
   $foreground-color: #148a55;
   $white: #ffffff;
   $black: #000;
+  $lighter-gray: #252525;
 
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    // display: flex;
-    // flex-direction: column;
-    margin: 0;
-    padding: 0;
-    background-color: $background-color;
-    font-family: 'Ubuntu', sans-serif;
-  }
-  .navbar {
-    display: flex;
-    position: fixed;
-    height: 100%;
-    width: 25rem;
-    background-color: #148a55;
-    align-items: center;
-    justify-content: flex-start;
-  }
-  ul {
-    display: flex;
-    list-style: none;
-    flex-direction: column;
-    font-size: 1.75rem;
-    margin-left: 5rem;
+// Global
+* {
+  font-family: sans-serif;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
 
-    li {
-      padding: 1rem;
+body {
+  padding: 0;
+  margin: 0;
+  background-color: $background-color;
+  font-family: 'Ubuntu', sans-serif;
+}
 
-      a {
-        text-decoration: none;
-        color: $white;
-        transition: 1s;
-        &:hover{
-          color: $background-color;
-          transition: 1s;
-        }
-      }
-    }
+// Page Layout
+.flex-container {
+  display: flex;
+  height: 100vh;
+  overflow: hidden;
+}
+
+// Side Navigation
+.navbar {
+  display: flex;
+  align-items: center;
+  background: $foreground-color;
+  // padding-top: 1.5rem;
+}
+
+.navbar ul {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  list-style: none;
+  flex-direction: column;
+  font-size: 1.75rem;
+}
+
+.navbar ul li a {
+  padding: 1rem 10rem;
+  font-size: 1.5rem;
+  color: $white;
+  text-decoration: none;
+  display: block;
+  transition: 1s;
+  &:hover{
+   color: $background-color;
+   transition: 1s;
   }
-  .container{
-    display: flex;
-    align-items: center;
-    background-color: #6c0d0d;
-    width: calc(100% - 25rem);
-    margin-left: 25rem;
-    height: 100%;
-    // flex: 1;
+}
+
+.container{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.introduction{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 30rem;
+  height: auto;
+  margin-bottom: 10rem;
+}
+.profilepic{
+  width: 250px;
+  height: 250px;
+  border-radius: 50%;
+  background-color: $lighter-gray;
+}
+.name{
+  font-size: 3rem;
+  color: $foreground-color;
+  line-height: 2;
+}
+.profession{
+  font-size: 1.5rem;
+  color: $white;
+}
+.github-btn{
+  text-decoration: none;
+  color: $white;
+  background-color: transparent;
+  padding: 15px 45px;
+  border-radius: 6px;
+  font-size: 1.2rem;
+  margin-top: 2rem;
+  border: 2px solid $foreground-color;
+  transition: 0.7s;
+  &:hover{
+    background-color: $foreground-color;
+    transition: 0.7s;
   }
+}
 </style>
