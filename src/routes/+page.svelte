1f4f32f0176d747dd97ef5ed250c704ea060b6c0
E3TU <99178022+E3TU@users.noreply.github.com>
2023-05-30 22:53:50 +0300

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,135 +1,177 @@
 <script>
-  let test = "hello world";
+  import Navbar from "./Navbar.svelte";
 </script>
 
 <body>
-  <div class="flex-container">
-    <div class="navbar">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Skills</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
+  <Navbar />
+
+  <div id="container" class="container">
+    <div class="introduction">
+      <img class="profilepic" src="default-profile.png" />
+      <h1 class="name">E3TU</h1>
+      <h1 class="profession">Software Developer</h1>
+      <a href="https://github.com/E3TU" class="github-btn">Check My Github</a>
     </div>
-    <div class="container">
-      <div class="introduction">
-        <img class="profilepic" src="default-profile.png">
-        <h1 class="name">E3TU</h1>
-        <h1 class="profession">Software Developer</h1>
-        <a href="https://github.com/E3TU" class="github-btn">Check My Github</a>
-      </div>
-    </div>
-    <div class="container2">
-      
-    </div>
+  </div>
+
+  <div id="container2" class="container2">
+    <h4>Info about me</h4>
+    <h1>About Me</h1>
+  </div>
+
+  <div id="container3" class="container3">
+    <h4>Why to choose me</h4>
+    <h1>Skills</h1>
+  </div>
+
+  <div id="container4" class="container4">
+    <h4>My work</h4>
+    <h1>Projects</h1>
+  </div>
+
+  <div id="container5" class="container5">
+    <h4>Get in touch</h4>
+    <h1>Contact</h1>
   </div>
 </body>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
+  @import url("https://fonts.googleapis.com/css2?family=Ubuntu&display=swap");
   $background-color: #191919;
   $foreground-color: #148a55;
   $white: #ffffff;
   $black: #000;
   $lighter-gray: #252525;
 
-// Global
-* {
-  font-family: sans-serif;
-  box-sizing: border-box;
-  padding: 0;
-  margin: 0;
-}
 
-body {
-  padding: 0;
-  margin: 0;
-  background-color: $background-color;
-  font-family: 'Ubuntu', sans-serif;
-}
-
-// Page Layout
-.flex-container {
-  display: flex;
-  height: 100vh;
-  overflow: hidden;
-}
-
-// Side Navigation
-.navbar {
-  display: flex;
-  align-items: center;
-  background: $foreground-color;
-  // padding-top: 1.5rem;
-}
-
-.navbar ul {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  list-style: none;
-  flex-direction: column;
-  font-size: 1.75rem;
-}
-
-.navbar ul li a {
-  padding: 1rem 10rem;
-  font-size: 1.5rem;
-  color: $white;
-  text-decoration: none;
-  display: block;
-  transition: 1s;
-  &:hover{
-   color: $background-color;
-   transition: 1s;
+  * {
+    font-family: sans-serif;
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
   }
-}
 
-.container{
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.introduction{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  width: 30rem;
-  height: auto;
-  margin-bottom: 10rem;
-}
-.profilepic{
-  width: 250px;
-  height: 250px;
-  border-radius: 50%;
-  background-color: $lighter-gray;
-}
-.name{
-  font-size: 3rem;
-  color: $foreground-color;
-  line-height: 2;
-}
-.profession{
-  font-size: 1.5rem;
-  color: $white;
-}
-.github-btn{
-  text-decoration: none;
-  color: $white;
-  background-color: transparent;
-  padding: 15px 45px;
-  border-radius: 6px;
-  font-size: 1.2rem;
-  margin-top: 2rem;
-  border: 2px solid $foreground-color;
-  transition: 0.7s;
-  &:hover{
-    background-color: $foreground-color;
+  body {
+    padding: 0;
+    margin: 0;
+    background-color: $background-color;
+    font-family: "Ubuntu", sans-serif;
+  }
+
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: calc(100% - 25rem);
+    margin-left: 25rem;
+    flex: 1;
+  }
+  .introduction {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 30rem;
+    height: auto;
+    margin-bottom: 10rem;
+  }
+  .profilepic {
+    width: 250px;
+    height: 250px;
+    border-radius: 50%;
+    background-color: $lighter-gray;
+  }
+  .name {
+    font-size: 3rem;
+    color: $foreground-color;
+    line-height: 2;
+  }
+  .profession {
+    font-size: 1.5rem;
+    color: $white;
+  }
+  .github-btn {
+    text-decoration: none;
+    color: $white;
+    background-color: transparent;
+    padding: 15px 45px;
+    border-radius: 6px;
+    font-size: 1.2rem;
+    margin-top: 2rem;
+    border: 2px solid $foreground-color;
     transition: 0.7s;
+    &:hover {
+      background-color: $foreground-color;
+      transition: 0.7s;
+    }
   }
-}
+  .container2 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: calc(100% - 25rem);
+    margin-left: 25rem;
+    h1 {
+      color: $white;
+      margin-top: 1rem;
+      font-size: 3rem;
+    }
+    h4{
+      margin-top: 2rem;
+      color: $foreground-color;
+    }
+  }
+  .container3{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: calc(100% - 25rem);
+    margin-left: 25rem;
+    h1 {
+      color: $white;
+      margin-top: 1rem;
+      font-size: 3rem;
+    }
+    h4{
+      margin-top: 2rem;
+      color: $foreground-color;
+    }
+  }
+  .container4{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: calc(100% - 25rem);
+    margin-left: 25rem;
+    h1 {
+      color: $white;
+      margin-top: 1rem;
+      font-size: 3rem;
+    }
+    h4{
+      margin-top: 2rem;
+      color: $foreground-color;
+    }
+  }
+  .container5{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: calc(100% - 25rem);
+    margin-left: 25rem;
+    h1 {
+      color: $white;
+      margin-top: 1rem;
+      font-size: 3rem;
+    }
+    h4{
+      margin-top: 2rem;
+      color: $foreground-color;
+    }
+  }
 </style>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,50 @@
+<header class="navbar">
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#container2">About me</a></li>
+      <li><a href="#container3">Skills</a></li>
+      <li><a href="#container4">Projects</a></li>
+      <li><a href="#container5">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+
+<style lang="scss">
+
+  $background-color: #191919;
+  $foreground-color: #148a55;
+  $white: #ffffff;
+  $black: #000;
+  $lighter-gray: #252525;
+
+  .navbar {
+    display: flex;
+    position: fixed;
+    height: 100%;
+    width: 25rem;
+    background-color: #148a55;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+     flex-direction: column;
+    flex-direction: column;
+    font-size: 1.75rem;
+    margin-left: 5rem;
+    li {
+      padding: 1rem;
+      a {
+        text-decoration: none;
+        color: $white;
+        transition: 1s;
+        &:hover{
+          color: $background-color;
+          transition: 1s;
+        }
+      }
+    }
+  }
+</style>
