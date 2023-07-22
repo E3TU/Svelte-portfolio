b07844ab4883ee5bbb46517afb624af836a5c198
E3TU <yes.0ztan@slmail.me>
2023-07-23 01:05:39 +0300

    Terminal window and buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,107 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>E3TU's portfolio</title>
+</svelte:head>
+
+<body>
+  <div class="flexbox">
+    <h1>E3TU</h1>
+    <div class="terminal">
+      <div class="titlebar">
+        <div class="buttons">
+          <button id="minimize" class="window-button" />
+          <button id="maximize" class="window-button" />
+          <button id="close" class="window-button" />
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+<style lang="scss">
+  @import url("https://fonts.googleapis.com/css2?family=Ubuntu&display=swap");
+
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    background-color: $background-color;
+    font-family: "Ubuntu", sans-serif;
+  }
+  .flexbox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    h1 {
+      margin-top: 3rem;
+      color: $foreground-color;
+      position: relative; /* Set position to relative to allow cursor positioning */
+      display: inline-block; /* To make the cursor pseudo-element inline */
+      &::after {
+        content: "▌"; /* Cursor content */
+        position: absolute;
+        bottom: 0;
+        left: 100%; /* Position cursor after the "User" text */
+        animation: blink 1s infinite; /* CSS animation for blinking effect */
+        color: $foreground-color;
+      }
+    }
+  }
+  @keyframes blink {
+    0%,
+    100% {
+      opacity: 1; /* Fully visible */
+    }
+    50% {
+      opacity: 0; /* Invisible */
+    }
+  }
+  .terminal {
+    background-color: $background-color;
+    margin-top: 3rem;
+    width: 60rem;
+    height: 45rem;
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+      rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+      rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  }
+  .titlebar {
+    display: flex;
+    justify-content: center;
+    background-color: #101010;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    width: 100%;
+    height: 2rem;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+  .buttons{
+    margin-right: 1rem;
+  }
+  .window-button {
+    width: .80rem;
+    height: .80rem;
+    border: none;
+    border-radius: 50%;
+    &:hover{
+      opacity: 0.8;
+    }
+  }
+  #close{
+    background-color: $red;
+  }
+  #maximize{
+    background-color: $yellow;
+  }
+  #minimize{
+    background-color: $green;
+  }
+</style>
