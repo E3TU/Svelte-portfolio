9d40172f76330e5078e79f2f5fe2ece6bb8a2a9d
E3TU <yes.0ztan@slmail.me>
2023-07-25 19:12:09 +0300

    Projects section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,6 +58,22 @@
       </div>
       <div class="projects">
         <h3>{name}:~$ <span class="whitetext">projects</span></h3>
+        <div class="project-item">
+          <h4>Android password generator</h4>
+          <a href="https://github.com/E3TU/Android-passwordgen">GitHub</a>
+        </div>
+        <div class="project-item">
+          <h4>Javascript todo app</h4>
+          <a href="https://github.com/E3TU/TodoApp">GitHub</a>
+        </div>
+        <div class="project-item">
+          <h4>Javascript clock app</h4>
+          <a href="https://github.com/E3TU/clock">GitHub</a>
+        </div>
+        <div class="project-item">
+          <h4>Bash random password generator</h4>
+          <a href="https://github.com/E3TU/Bash-random-password-generator">GitHub</a>
+        </div>
       </div>
       <div class="contact" />
     </div>
@@ -185,5 +201,26 @@
   }
   .flex-item{
     width: 8rem;
+  }
+  .projects{
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    h3{
+      width: 100%;
+    }
+    h4{
+      margin-left: 1rem;
+      color: $foreground-color;
+    }
+    a{
+      margin-left: 1rem;
+      background-color: transparent;
+      color: $white;
+      font-size: 0.8rem;
+    }
+  }
+  .project-item{
+    width: auto;
   }
 </style>
